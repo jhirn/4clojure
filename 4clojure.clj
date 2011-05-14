20432c36d7d0ef4f899d89ff23ec96999f95662c
Joe Hirn <joseph.hirn@gmail.com>
2011-05-14 01:54:59 -0500

    Drop Every Nth Item

--- 4clojure.clj
+++ 4clojure.clj
@@ -5,18 +5,31 @@
                     (map first 
                          (iterate (fn [[x y]] [y (+ x y)]) [1 1]))))
 ;; #38 -Max
-(def my-max (fn this ([x] x)
+(def my-max
+     (fn this ([x] x)
               ([x y] (if (> x y) x y))
               ([x y & more] (reduce this (this x y) more))))
 
 ;; #39-Interleave
-(def my-interleave (fn this [x y]
+(def my-interleave
+     (fn this [x y]
                      (when (and (first x) (first y))
                        (cons (first x) (cons (first y) (this (rest x)(rest y)))))))
 
 ;; #40-Interpose
-(def my-interpose (fn this [c xs]
+(def my-interpose
+     (fn this [c xs]
                     (when-let [n (first xs)]
                       (if (second xs)
                         (cons n (cons c (this c (rest xs)))) 
                         (cons n (this c (rest xs)))))))
+
+;; #41-Drop Every Nth Item
+(def drop-every-nth
+     (fn [xs n]
+       (filter (complement nil?)
+               (map-indexed 
+                #(if (not= 0 (rem (inc %1)  n))%2)
+                    xs))))
+
+
