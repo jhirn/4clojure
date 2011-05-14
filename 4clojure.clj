5e8248c0aa21b6e88d14325094763139139a4a37
Joe Hirn <joseph.hirn@gmail.com>
2011-05-13 21:05:33 -0500

    Added Max and and Fib from history

--- 4clojure.clj
+++ 4clojure.clj
@@ -1,9 +1,18 @@
 (ns user)
 
+;; #26-Fibonacci
+(def my-fib # (take % 
+                    (map first 
+                         (iterate (fn [[x y]] [y (+ x y)]) [1 1]))))
+;; #38 -Max
+(def my-max (fn this ([x] x)
+              ([x y] (if (> x y) x y))
+              ([x y & more] (reduce this (this x y) more))))
+
 ;; #39-Interleave
 (def my-interleave (fn this [x y]
-           (when (and (first x) (first y))
-             (cons (first x) (cons (first y) (this (rest x)(rest y)))))))
+                     (when (and (first x) (first y))
+                       (cons (first x) (cons (first y) (this (rest x)(rest y)))))))
 
 ;; #40-Interpose
 (def my-interpose (fn this [c xs]
