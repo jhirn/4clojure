20d60b1c1ea0f3143668b5e24a52a4dd8b0a4306
Joe Hirn <joseph.hirn@gmail.com>
2011-10-23 13:09:35 -0500

    upgraded to clojure 1.3

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject four-clojure "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure-contrib "1.3.0"]]
+  :dev-dependencies [[swank-clojure "1.3.3"]])
 
